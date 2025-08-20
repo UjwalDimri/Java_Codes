279c8fc7045c9cf0f6ddcf7aa689bddd91cc6bc2
Ujwal dimri <ujwaldimri223@gmail.com>
2025-08-20 18:09:59 +0530

    java

---
+++ javabasics.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class javabasics{ 
+  /*public static void floyds_triangle(int n){
+    //outer
+    int counter = 1;
+    for(int i =1; i<=n;i++){
+      //inner how many times will counter be printed
+      for(int j=1;j<=i;j++){
+          System.out.print(counter + " ");
+            counter++;
+      }
+        System.out.println();
+
+    }
+      
+        
+        
+  }/* */
+  /*public static void zero_one_triangle(int n){
+    for (int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        if((i+j)%2==0){ //even
+         System.out.print("1");
+
+
+        }else{
+          System.out.print("0");
+        }
+      }
+    System.out.println(); 
+    }
+  
+  }  */
+
+   
+  
+  public static void main(String args[]) { 
+          //floyds_triangle(100);
+          zero_one_triangle(20);
+
+     
+
+
+
+  }   
+}    
+       
+        
